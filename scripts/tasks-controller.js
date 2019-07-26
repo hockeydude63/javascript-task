3948c96477ea6d6ff726f44790541a9f5896da6b
Stewart Terry <44471071+hockeydude63@users.noreply.github.com>
2019-07-25 22:07:23 -0600

    Moved to Web Server

---
+++ scripts/tasks-controller.js
@@ -0,0 +1,39 @@
+tasksController = function() {
+    var taskPage;
+    var initialised = false;
+
+    return {
+        init:function(page) {
+            if (!initialised) {
+                taskPage = page;
+                $(taskPage).find('[required="required"]').prev('label').append('<span>*</span>').children('span').addClass('required');
+
+                $(taskPage).find('tbody tr:even').addClass('even');
+
+                $(taskPage).find('#btnAddTask').click(function(evt) {
+                    evt.preventDefault();
+                    $(taskPage).find('#taskCreation').removeClass('not');
+                });
+
+                $(taskPage).find('tbody tr').click(function(evt) {
+                    $(evt.targetn).closest('td').siblings().addBack().toggleClass('rowHighlight');
+                });
+
+                $(taskPage).find('#tblTasks tbody').on('click', '.deleteRow', function (evt) {
+                    evt.preventDefault;
+                    $(evt.target).parents('tr').remove();
+                });
+
+                $(taskPage).find('#saveTask').click(function (evt) {
+                    evt.preventDefault();
+                    if($(taskPage).find('form').valid()) {
+                        var task = $('form').toObject();
+                        $('#taskRow').tmpl(task).appendTo($('#tblTasks tbody'));
+                    }
+                });
+                
+                initialised = true;
+            }
+        }
+    }
+}();
